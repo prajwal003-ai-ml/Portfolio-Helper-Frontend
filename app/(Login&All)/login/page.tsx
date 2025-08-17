56b04515003b654c6a17d6e25a874d6c2a09dc8a
PrajwalNeupaneDgaf <157794663+PrajwalNeupaneDgaf@users.noreply.github.com>
2025-08-17 23:29:23 +0545

    All Done

--- app/(Login&All)/login/page.tsx
+++ app/(Login&All)/login/page.tsx
@@ -46,14 +46,14 @@ const page = () => {
 
   const [LoadingForget, setLoadingForget] = useState(false)
 
-  const sendEmail = async()=>{
+  const sendEmail = async () => {
 
-    if(LoadingForget){
+    if (LoadingForget) {
       return
     }
-    if(!Email){
+    if (!Email) {
       toast('Please add Email and Click Forget Password')
-      return 
+      return
     }
     try {
       setLoadingForget(true)
@@ -61,11 +61,11 @@ const page = () => {
       const send = await api.get(`/auth/send-email-forget-password/${Email}`)
 
       toast.success('Please Check Your Email')
-      
+
     } catch (error) {
       toast.error('Failed To Send Email , Try Remebering Password')
     }
-    finally{
+    finally {
       setLoadingForget(false)
     }
   }
@@ -95,13 +95,13 @@ const page = () => {
             Don't have an account? <Link href='/register' className='text-orange-700'>Register</Link>
           </p>
         </div>
-        <button  className='w-full p-2 border border-gray-700 rounded bg-[#ffffff18] text-white cursor-pointer hover:bg-[#ffffff2c]' type="submit">
+        <button className='w-full p-2 border border-gray-700 rounded bg-[#ffffff18] text-white cursor-pointer hover:bg-[#ffffff2c]' type="submit">
           {Loading ? 'Logging in...' : 'Login'}
         </button>
         <div className='text-sm text-gray-500 mt-2 text-center'>
-          <div onClick={sendEmail} className='text-orange-700 cursor-pointer'>{LoadingForget?"Sending Email":"Forgot Password?"}</div>
         </div>
       </form>
+      <div onClick={sendEmail} className='text-orange-700 cursor-pointer'>{LoadingForget ? "Sending Email" : "Forgot Password?"}</div>
     </div>
   )
 }
